8b58083de8daa7fc71ca94949f71f0910d92007a
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-12-01 23:02:14 +0530

    fix: add missing route handler for serving index.html

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,11 @@ import { ENV } from "./lib/env.js";
 const app = express();
 const __dirname = path.resolve();
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+}
+);
+
 app.get("/happy", (req, res) => {
   res.json({ message: "Server is running" });
 });
